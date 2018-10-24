aa59f6e486d6840699f736171fb62c1944d42b4d
Dominik Biedebach <6538827+bdbch@users.noreply.github.com>
2018-10-24 23:34:37 +0200

    Added Support for Styled Components and added Variables

---
+++ _client/GlobalHead.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GlobalHead = () => (
+  <React.Fragment>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <meta robots="noindex, nofollow" />
+    <link rel="icon" href="/static/favicon.png" type="image/png" />
+  </React.Fragment>
+)
+
+export default GlobalHead
---
+++ _client/src/Global/Variables/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+  white: '#fff',
+  text: '#222',
+  primary: '#ff0067',
+}
+
+export default colors
